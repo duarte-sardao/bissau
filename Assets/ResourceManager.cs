28d791f0d8787389b803bdb3b34d89b998c888bc
Dudugs <up201905497@fe.up.pt>
2023-04-15 17:47:20 +0100

    localization on the tooltip

--- Assets/ResourceManager.cs
+++ Assets/ResourceManager.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Metadata;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
 
 public class ResourceManager : GlobalVars
 {
@@ -22,6 +26,9 @@ public class ResourceManager : GlobalVars
 
     [SerializeField] private PathCalculator path;
 
+    public LocalizedStringTable m_StringTable;
+
+
     void Start()
     {
         UpdateBoard();
@@ -39,45 +46,42 @@ public class ResourceManager : GlobalVars
 
     void GetInputs()
     {
+        //terribleness i could do this with a mere smart string but i dont wanna do that dog itd be such a mess to wrangle
+        //just imagine having add to one ok maybe not that difficult but a massive block of formatters for each one unredeable
+        //the big gain would be not having to wait for the tick for the language to update but why would you be updating language midgame
         Dictionary<string, int> pol = new Dictionary<string, int>()
         {
-            { "Base Value", 5},
-            { "Sectors", 0},
-            { "Buildings", 0},
-            { "Bordering Ally", 0},
+            { "BASE_VALUE", 5},
+            { "SECTORS", 0},
+            { "BUILDINGS", 0},
+            { "BORDERING", 0},
         };
-        Dictionary<string, int> mon = new Dictionary<string, int>()
-        {
-            { "Base Value", 5},
-            { "Sectors", 0},
-            { "Buildings", 0},
-            { "Bordering Ally", 0},
-        };
+        Dictionary<string, int> mon = new Dictionary<string, int>(pol);
         lastmon = 0;
         lastpol = 0;
         foreach(var sect in g_sects)
         {
             if(sect.ControlLevel <= g_fullcontrolg)
             {
-                mon["Sectors"] += 1;
-                pol["Sectors"] += 1;
+                mon["SECTORS"] += 1;
+                pol["SECTORS"] += 1;
                 if (sect.buildings["school"].built)
                 {
-                    pol["Buildings"] += g_schoolgain;
-                    mon["Buildings"] -= g_buildingcost;
+                    pol["BUILDINGS"] += g_schoolgain;
+                    mon["BUILDINGS"] -= g_buildingcost;
                 }
                 if (sect.buildings["hospital"].built)
-                    mon["Buildings"] -= g_buildingcost;
+                    mon["BUILDINGS"] -= g_buildingcost;
                 if (sect.buildings["farm"].built)
-                    mon["Buildings"] += g_farmgain;
+                    mon["BUILDINGS"] += g_farmgain;
                 if (sect.buildings["camp"].built)
-                    mon["Buildings"] -= g_campcost;
+                    mon["BUILDINGS"] -= g_campcost;
             }
         }
         if (path.NeighboursForeign())
         {
-            mon["Bordering Ally"] += 5;
-            pol["Bordering Ally"] += 5;
+            mon["BORDERING"] += 5;
+            pol["BORDERING"] += 5;
         }
         polinfo.text = "";
         moninfo.text = "";
@@ -103,10 +107,10 @@ public class ResourceManager : GlobalVars
         string badtext = "";
         if(value > 0)
         {
-            txt.text += "<color=green>" + name + ": +" + value + "\n"; 
+            txt.text += "<color=#008F00>" + m_StringTable.GetTable().GetEntry(name).LocalizedValue + ": +" + value + "\n"; 
         } else if(value < 0)
         {
-            badtext = "<color=red>" + name + ": " + value + "\n";
+            badtext = "<color=#8F0000>" + m_StringTable.GetTable().GetEntry(name).LocalizedValue + ": " + value + "\n";
         }
         return badtext;
     }
