973b8a6fa2fdb55adc28fafc827091416f583d41
Dudugs <up201905497@fe.up.pt>
2023-08-27 20:28:15 +0300

    fix retreat bug (how did it even happen?? did i never use the health target check). added marker for bissau city

--- Assets/EnemyUnitLogic.cs
+++ Assets/EnemyUnitLogic.cs
@@ -19,14 +19,18 @@ public class EnemyUnitLogic : UnitLogic
 
     private void CheckMove()
     {
+        if (moving || (healthTarget > health && this.sector.friend == null))
+            return;
         if(this.health < 0.15f && (this.sector.ControlLevel <= g_fullcontrolp || this.sector.friend != null))
         {
             calc.GetRetreatForEnemy(this.sector);
             healthTarget = Random.Range(0.7f, 1);
+            Debug.Log("retreating and healing until " + healthTarget);
         } else if(this.health >= healthTarget && this.sector.ControlLevel >= g_fullcontrolp)
         {
             calc.GetSpotForEnemy(this.sector);
             healthTarget = 0f;
+            Debug.Log("attacking");
         }
     }
 }
